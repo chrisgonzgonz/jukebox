956d8b9752a5b55707a744f61f1202f6639ef369
Chris Gonzales <gonzales.chris.a@gmail.com>
2013-06-17 07:45:59 -0400

    refactored for object orientation

--- jukebox.rb
+++ jukebox.rb
@@ -1,5 +1,58 @@
 # jukebox.rb
 
+class Jukebox
+  attr_accessor :songs
+
+  def initialize (songs)
+    @songs = songs
+  end
+
+
+  # list, play, help, exit
+  def list
+    i = 1
+    songs.each do |song|
+      puts "#{i} #{song}"
+      i = i+1
+    end
+  end
+
+  private
+  def open_video (index)
+  links = [ 
+    "http://www.youtube.com/watch?v=HL548cHH3OY",
+    "http://www.youtube.com/watch?v=VARmJlPu6i4",
+    "http://www.youtube.com/watch?v=K0g7R3xqdcM",
+    "http://www.youtube.com/watch?v=0YuSg4mts9E",
+    "http://www.youtube.com/watch?v=KkWm898ileE",
+    "http://www.youtube.com/watch?v=9i1MXHGB8g0",
+    "https://www.youtube.com/watch?v=gnkVUReUVpQ"
+  ]
+
+  `open #{links[index]}`
+  end
+
+  public
+  def play
+    selection = 0
+    unless (selection >= 1) && (selection <= songs.length)
+      puts "Please enter the numerical selection of your song."
+      selection = gets.chomp
+      selection = selection.to_i - 1
+    end
+    puts "Now playing: #{songs[selection]}"
+    open_video(selection)
+  end
+
+  def helpme
+    puts "Welcome to jukebox. 
+    You can 'list' to see your songs. 
+    You can 'play' to play a song. 
+    You can ask for 'help' to see this again. 
+    And you can 'exit' to leave!"
+  end
+end
+
 
 songs = [
   "The Phoenix - 1901",
@@ -11,71 +64,25 @@ songs = [
   "The Phoenix - Consolation Prizes"
 ]
 
+jukebox = Jukebox.new(songs)
 
-# list, play, help, exit
-
-
-# list
-
-def list(songs)
-  i = 1
-  songs.each do |song|
-    puts "#{i} #{song}"
-    i = i+1
-  end
-end
-
-def open_video(index)
-links = [ 
-  "http://www.youtube.com/watch?v=HL548cHH3OY",
-  "http://www.youtube.com/watch?v=VARmJlPu6i4",
-  "http://www.youtube.com/watch?v=K0g7R3xqdcM",
-  "http://www.youtube.com/watch?v=0YuSg4mts9E",
-  "http://www.youtube.com/watch?v=KkWm898ileE",
-  "http://www.youtube.com/watch?v=9i1MXHGB8g0",
-  "https://www.youtube.com/watch?v=gnkVUReUVpQ"
-]
-
-`open #{links[index]}`
-end
-
-def play(songs)
-  selection = 0
-  unless (selection >= 1) && (selection <= songs.length)
-    puts "Please enter the numerical selection of your song."
-    selection = gets.chomp
-    selection = selection.to_i - 1
-  end
-  puts "Now playing: #{songs[selection]}"
-  open_video(selection)
-end
-
-def helpme
-  puts "Welcome to jukebox. 
-  You can 'list' to see your songs. 
-  You can 'play' to play a song. 
-  You can ask for 'help' to see this again. 
-  And you can 'exit' to leave!"
-end
-
-
-playing = true
+jukebox_on = true
 entry = 0
 helpme
 
-while playing 
+while jukebox_on 
   puts "Tell us what you'd like to do:"
   entry = gets.strip.downcase
   case entry
     when "list"
-      list(songs)
+      jukebox.list
     when "play"
-      play(songs)
+      jukebox.play
     when "help"
-      helpme
+      jukebox.helpme
     when "exit"
-      playing = false
+      jukebox_on = false
     else
-      helpme
+      jukebox.helpme
   end
 end
